fe4ac07cafc66861f083ae588101213871d6f937
Piyushap <Piyushap@corp.rpqb.com>
2017-06-17 18:48:53 +0530

    changes

--- app/mixins/stepper.js
+++ app/mixins/stepper.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
     selectedAnchor : null,
     selectedVendor : null,
+    selectedvendr:[],
+    selectedvendr :Ember.A(), 
+    
     actions:{
         chooseAnchor:function(anchorItem){
             this.set('selectedAnchor',anchorItem);
@@ -15,13 +18,16 @@ export default Ember.Mixin.create({
             }
         },
         chooseVendor:function(vendorItem){
-             this.set('selectedVendor',vendorItem);
+           /*  this.set('selectedVendor',vendorItem);
             this.set('vendornotchoosen', false);
             this.set('percentageComplete', 60);
             var currentStep = this.get('currentStep');
             if(currentStep!=3 ){
                     this.send('nextStep');
-            }
+            }*/
+             var selectvendorlist=[];
+            selectvendorlist=vendorItem;
+            console.log("selectvendorlist"+selectvendorlist);
             
         },
         saveModel: function(){
@@ -36,6 +42,11 @@ export default Ember.Mixin.create({
             var currentStep = this.get('currentStep');
             currentStep = currentStep - 1;
             this.set('currentStep',currentStep);
+        },
+        submitselectvendorlist:function(selectvendor){
+            var selectvendor = selectvendor;
+            console.log('selectvendor'+selectvendor);
+                console.log('this is done');
         }
     }
 });
